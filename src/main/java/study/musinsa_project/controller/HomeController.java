904b014ee24af39f8875221f3bdf4d500da5289f
fhfld <seehb126@naver.com>
2024-10-29 11:19:20 +0900

    ✨feat: project 초기 세팅

--- src/main/java/study/musinsa_project/controller/HomeController.java
+++ src/main/java/study/musinsa_project/controller/HomeController.java
@@ -1,8 +1,16 @@
 package study.musinsa_project.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "";
+    }
 }
