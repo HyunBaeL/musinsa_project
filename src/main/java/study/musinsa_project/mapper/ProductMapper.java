41c6f386384dff972b2b669fd2a9b382b9b7cfdd
jeongwooa <ljw6027@naver.com>
2024-11-05 17:27:38 +0900

    ✨feat : 배포준비

--- src/main/java/study/musinsa_project/mapper/ProductMapper.java
+++ src/main/java/study/musinsa_project/mapper/ProductMapper.java
@@ -2,22 +2,29 @@ package study.musinsa_project.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import study.musinsa_project.dto.product.ProductRegisterRequestDto;
 import study.musinsa_project.dto.product.ProductRegisterResponseDto;
 import study.musinsa_project.dto.product.ProductSummaryDto;
 import study.musinsa_project.entity.Product;
 
-@Mapper(componentModel = "spring")
-public interface ProductMapper
-{
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ProductMapper {
 
-    // ProductRegisterDto 를 Product 엔티티로 매핑
+    // ProductRegisterDto를 Product 엔티티로 매핑
+    @Mapping(target = "id", ignore = true) // id는 무시
+    @Mapping(target = "user", ignore = true) // user는 무시
+    @Mapping(target = "startDate", ignore = true) // startDate는 무시
+    @Mapping(target = "state", ignore = true) // state는 무시
+    @Mapping(target = "cartItems", ignore = true) // cartItems는 무시
     Product toEntity(ProductRegisterRequestDto productRegisterRequestDto);
 
-    // product 엔티티를 summaryDto 로 매핑
+    // product 엔티티를 summaryDto로 매핑
     ProductSummaryDto toSummaryDto(Product product);
 
+    // product 엔티티를 ProductRegisterResponseDto로 매핑
     @Mapping(source = "product.id", target = "productId")
     @Mapping(source = "product.itemName", target = "productName")
+    @Mapping(target = "message", ignore = true) // message는 무시
     ProductRegisterResponseDto toResponseDto(Product product);
 }
