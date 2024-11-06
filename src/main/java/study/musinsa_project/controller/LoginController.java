2cbbddcb770c2ca3706d726f992d0b94985764a6
[dy6379] <[telly6379@naver.com]>
2024-11-06 15:31:15 +0900

    로그인 수정

--- src/main/java/study/musinsa_project/controller/LoginController.java
+++ src/main/java/study/musinsa_project/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import study.musinsa_project.service.LoginService;
 import study.musinsa_project.dto.Login;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api")
@@ -13,8 +15,8 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping(value = "/login")
-    public String login(@RequestBody Login loginRequest) {
-        String message = loginService.login(loginRequest);
-        return "로그인 성공! 토큰:" + message;
+    public Map<String, String> login(@RequestBody Login loginRequest) {
+        String token =  loginService.login(loginRequest);
+        return Map.of("token", token);
     }
 }
